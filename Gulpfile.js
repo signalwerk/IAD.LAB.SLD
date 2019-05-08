fd7091b251e4cf94995ae7f5b64b91a651a7552b
Stefan Huber <sh@signalwerk.ch>
2019-05-08 02:18:40 +0200

    MOD: switch to gulp 4

--- Gulpfile.js
+++ Gulpfile.js
@@ -20,51 +20,63 @@ var sassOptions = {
   outputStyle: "expanded"
 };
 
-gulp.task("copyRevealJs", function() {
-  gulp.src(revealRoot + "/js/*").pipe(gulp.dest(output + "/js/"));
-  gulp.src(revealRoot + "/plugin/**/*").pipe(gulp.dest(output + "/plugin/"));
-  gulp.src(revealRoot + "/lib/**/*").pipe(gulp.dest(output + "/lib/"));
-});
+gulp.task(
+  "copyRevealJs",
+  gulp.parallel(function(done) {
+    gulp.src(revealRoot + "/js/*").pipe(gulp.dest(output + "/js/"));
+    gulp.src(revealRoot + "/plugin/**/*").pipe(gulp.dest(output + "/plugin/"));
+    gulp.src(revealRoot + "/lib/**/*").pipe(gulp.dest(output + "/lib/"));
 
-gulp.task("copyIMG", function() {
-  gulp.src(imagePath).pipe(gulp.dest(output + "/data/"));
-  gulp.src("./data/media/**/*").pipe(gulp.dest(output + "/data/media/"));
-});
+    done();
+  })
+);
 
-gulp.task("connect", function() {
-  connect.server({
-    root: "./build/",
-    livereload: true
-  });
-});
+gulp.task(
+  "copyIMG",
+  gulp.parallel(function(done) {
+    gulp.src(imagePath).pipe(gulp.dest(output + "/data/"));
+    gulp.src("./data/media/**/*").pipe(gulp.dest(output + "/data/media/"));
+    done();
+  })
+);
 
-gulp.task("md", function() {
-  gulp
-    .src("./data/*/*/*.md")
-    .pipe(gulp.dest(output + "/data/"))
-    .pipe(
-      // do a subtask based on the md
-      flatmap(function(stream, file) {
-        var currentDirectory = path.dirname(file.path);
-        var relativePath = path.relative(process.cwd(), currentDirectory);
+gulp.task(
+  "md",
+  gulp.parallel(function(done) {
+    gulp
+      .src("./data/*/*/*.md")
+      .pipe(gulp.dest(output + "/data/"))
+      .pipe(
+        // do a subtask based on the md
+        flatmap(function(stream, file) {
+          var currentDirectory = path.dirname(file.path);
+          var relativePath = path.relative(process.cwd(), currentDirectory);
 
-        gulp.src("./src/slides/index.html").pipe(gulp.dest(relativePath));
+          gulp.src("./src/slides/index.html").pipe(gulp.dest(relativePath));
 
-        return stream;
-      })
-    )
-    .pipe(connect.reload());
-});
+          return stream;
+        })
+      )
+      .pipe(connect.reload());
+    done();
+  })
+);
 
-gulp.task("index", function(obj) {
-  gulp
-    .src(indexHTML)
-    .pipe(gulp.dest(output))
-    .pipe(connect.reload());
-});
+gulp.task(
+  "index",
+  gulp.parallel(function(done) {
+    gulp
+      .src(indexHTML)
+      .pipe(gulp.dest(output))
+      .pipe(connect.reload());
+    done();
+  })
+);
 
-gulp.task("sass", function() {
-  return (
+gulp.task(
+  "sass",
+  gulp.parallel(function(done) {
+    // return (
     gulp
       .src(themeSCSS)
       // .pipe(sourcemaps.init())
@@ -76,41 +88,70 @@ gulp.task("sass", function() {
 
       // Release the pressure back and trigger flowing mode (drain)
       // See: http://sassdoc.com/gulp/#drain-event
-      .resume()
-  );
-});
+      .resume();
+    // );
+    done();
+  })
+);
 
-gulp.task("watch", function() {
-  gulp.watch(themeSCSS, ["sass"]);
-  gulp.watch(indexHTML, ["index"]);
-
-  gulp.watch(imagePath).on("change", function(file) {
-    gulp
-      .src(file.path, { base: "." })
-      .pipe(gulp.dest(output))
-      .pipe(connect.reload());
-  });
-
-  gulp
-    .watch(["./data/*/*/*.md"])
-    .on("change", function(file) {
-      gulp
-        .src(file.path, { base: "." })
-        .pipe(gulp.dest(output))
-        .pipe(connect.reload());
-    })
-    .on("change", function(file) {
-      // Workaround to keep original folder structure
-      var currentDirectory = path.dirname(file.path);
-      var relativePath = path.relative(process.cwd(), currentDirectory);
-
-      gulp
-        .src("./src/slides/index.html")
-        .pipe(gulp.dest(output + "/" + relativePath))
-        .pipe(connect.reload());
+gulp.task(
+  "connect",
+  gulp.parallel(function(done) {
+    connect.server({
+      root: "./build/",
+      livereload: true
     });
-});
+    done();
+  })
+);
 
-gulp.task("default", ["build", "connect", "watch"]);
+gulp.task(
+  "watch",
+  gulp.parallel(function(done) {
+    gulp.watch(themeSCSS, gulp.parallel("sass"));
+    gulp.watch(indexHTML, gulp.parallel("index"));
 
-gulp.task("build", ["copyRevealJs", "sass", "md", "index", "copyIMG"]);
+    gulp.watch(imagePath).on(
+      "change",
+      gulp.parallel(function(file) {
+        gulp
+          .src(file.path, { base: "." })
+          .pipe(gulp.dest(output))
+          .pipe(connect.reload());
+      })
+    );
+
+    gulp
+      .watch(["./data/*/*/*.md"])
+      .on(
+        "change",
+        gulp.parallel(function(file) {
+          gulp
+            .src(file.path, { base: "." })
+            .pipe(gulp.dest(output))
+            .pipe(connect.reload());
+        })
+      )
+      .on(
+        "change",
+        gulp.parallel(function(file) {
+          // Workaround to keep original folder structure
+          var currentDirectory = path.dirname(file.path);
+          var relativePath = path.relative(process.cwd(), currentDirectory);
+
+          gulp
+            .src("./src/slides/index.html")
+            .pipe(gulp.dest(output + "/" + relativePath))
+            .pipe(connect.reload());
+        })
+      );
+    done();
+  })
+);
+
+gulp.task(
+  "build",
+  gulp.parallel("copyRevealJs", "sass", "md", "index", "copyIMG")
+);
+
+gulp.task("default", gulp.parallel("build", "connect", "watch"));
