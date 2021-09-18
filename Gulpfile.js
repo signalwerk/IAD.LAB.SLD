c82eed2c1022bd8cb72216328de8202b8faa71f5
signalwerk <sh@signalwerk.ch>
2021-09-18 07:23:04 +0200

    ADD: KW37

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,11 +1,11 @@
 const gulp = require("gulp");
-const sass = require("gulp-sass");
 const sourcemaps = require("gulp-sourcemaps");
 const autoprefixer = require("gulp-autoprefixer");
 const watch = require("gulp-watch");
 const path = require("path");
 var flatmap = require("gulp-flatmap");
 const connect = require("gulp-connect");
+var sass = require('gulp-sass')(require('sass'));
 
 const output = "./build";
 const revealRoot = "./node_modules/reveal.js/";
